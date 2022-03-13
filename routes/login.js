af4e38fe2d2b6ee82c87d557d83841edb3d18549
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-03-13 18:22:37 -0400

    add admin check

--- routes/login.js
+++ routes/login.js
@@ -31,7 +31,24 @@ module.exports = (db) => {
               }
             });
         } else {
-          return res.status(403).send("<h1>400</h1><h2>Email or password does not exist or incorrect</h2>");
+          database.getAdminEmail(email)
+            .then((value) => {
+              if (value) {
+                return db
+                  .query(`SELECT password FROM admins WHERE email = $1`, [email])
+                  .then((responds) => {
+                    const hashedPassword = responds.rows[0].password;
+                    console.log("stuff i guess", responds.rows[0].password, bcrypt.compareSync(password, hashedPassword));
+                    if (bcrypt.compareSync(password, hashedPassword)) {
+                      res.redirect("/");
+                    } else {
+                      res.status(403).send("<h1>403</h1><h2>Email or password does not exist or incorrect</h2>");
+                    }
+                  });
+              } else {
+                res.status(403).send("<h1>400</h1><h2>Email or password does not exist or incorrect</h2>");
+              }
+            });
         }
       });
   });
