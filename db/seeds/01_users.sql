6859258ffaa150a2af7911de617376010dd42691
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-03-13 18:27:22 -0400

    Insert data

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,10 +1,10 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name, email, password, is_admin) VALUES ('Alice', 'alice@g', 'password', TRUE);
-INSERT INTO users (name, email, password, is_admin)  VALUES ('Kira', 'Kira8945@gmail.com', 'password', TRUE);
+INSERT INTO users (name, email, password) VALUES ('Alice', 'alice@g', 'password');
+INSERT INTO users (name, email, password)  VALUES ('Kira', 'Kira8945@gmail.com', 'password');
 INSERT INTO users (name, email, password)  VALUES ('James', 'james@g', 'password');
 INSERT INTO users (name, email, password)  VALUES ('Amanda', 'amanda@', 'password');
-INSERT INTO users (name, email, password, is_admin) VALUES ('Ab', 'jayda.mraz@hotmail.com', 'password', TRUE);
-INSERT INTO users (name, email, password, is_admin) VALUES ('Aba', 'savanna58@yahoo.com', 'password', TRUE);
+INSERT INTO users (name, email, password) VALUES ('Ab', 'jayda.mraz@hotmail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Aba', 'savanna58@yahoo.com', 'password');
 INSERT INTO users (name, email, password) VALUES ('Abad', 'iheaney@anderson.com', 'password');
 INSERT INTO users (name, email, password) VALUES ('Abagael', 'pdoyle@yahoo.com', 'password');
 INSERT INTO users (name, email, password) VALUES ('Abagail', 'hturcotte@gmail.com', 'password');
@@ -17,42 +17,33 @@ INSERT INTO users (name, email, password) VALUES ('Abbey', 'kali57@yahoo.com', '
 INSERT INTO users (name, email, password) VALUES ('Abbi', 'nblick@johnston.com', 'password');
 INSERT INTO users (name, email, password) VALUES ('Abbie', 'icummerata@gmail.com', 'password');
 
-INSERT INTO items (user_id, title, description, thumbnail_photo_url, cover_photo_url, cost, date, sold, country, city, province)
+INSERT INTO admins (name, email, password) VALUES ('Blice', 'blice@g', 'password');
+INSERT INTO admins (name, email, password)  VALUES ('Bira', 'bira8945@gmail.com', 'password');
+INSERT INTO admins (name, email, password)  VALUES ('Bames', 'bames@g', 'password');
+INSERT INTO admins (name, email, password)  VALUES ('Bmanda', 'bmanda@', 'password');
+INSERT INTO admins (name, email, password) VALUES ('bb', 'jay.mraz@hotmail.com', 'password');
+INSERT INTO admins (name, email, password) VALUES ('Pba', 'savan@yahoo.com', 'password');
+
+
+
+
+INSERT INTO items (admin_id, title, description, thumbnail_photo_url, cover_photo_url, cost, date, sold, country, city, province)
 VALUES (1, 'shoe', 'black', '/image', '/image2', 100, '2022-03-11', FALSE, 'Canada', 'Toronto', 'ON');
 
-INSERT INTO items (user_id, title, description,thumbnail_photo_url, cover_photo_url, cost, date, sold, country, city, province)
-VALUES (2, 'hat', 'white', '/image3', '/image4', 50, '2022-03-05', True, 'Canada', 'Montreal', 'QC');
+INSERT INTO items (admin_id, title, description, thumbnail_photo_url, cover_photo_url, cost, date, sold, country, city, province)
+VALUES (1, 'hat', 'white', '/image3', '/image4', 50, '2022-03-05', True, 'Canada', 'Montreal', 'QC');
 
-INSERT INTO items (user_id, title, description,thumbnail_photo_url, cover_photo_url, cost, date, sold, country, city, province)
+INSERT INTO items (admin_id, title, description,thumbnail_photo_url, cover_photo_url, cost, date, sold, country, city, province)
+VALUES (3, 'shirt','', '/image5', '/image6', 80, '2022-01-15', FALSE, 'Canada', 'London', 'ON');
 
-VALUES (1, 'shirt','', '/image5', '/image6', 80, '2022-01-15', FALSE, 'Canada', 'London', 'ON');
+INSERT INTO items (admin_id, title,description, thumbnail_photo_url, cover_photo_url, cost, date, sold, country, city, province)
+VALUES (4, 'dress', 'description','/image', '/image2', 25, '2022-03-09', FALSE, 'Canada', 'Toronto', 'ON');
 
-INSERT INTO items (user_id, title,description, thumbnail_photo_url, cover_photo_url, cost, date, sold, country, city, province)
-VALUES (1, 'dress', 'description','/image', '/image2', 25, '2022-03-09', FALSE, 'Canada', 'Toronto', 'ON');
+INSERT INTO items (admin_id, title,description, thumbnail_photo_url, cover_photo_url, cost, date, sold, country, city, province)
+VALUES (5, 'pants', 'demim', '/image3', '/image4', 65, '2022-02-05', True, 'Canada', 'Montreal', 'QC');
 
-INSERT INTO items (user_id, title,description, thumbnail_photo_url, cover_photo_url, cost, date, sold, country, city, province)
-VALUES (4, 'pants', 'demim', '/image3', '/image4', 65, '2022-02-05', True, 'Canada', 'Montreal', 'QC');
-
-INSERT INTO items (user_id, title,description, thumbnail_photo_url, cover_photo_url, cost, date, sold, country, city, province)
-VALUES (5, 'shirt', 'graphic text' , '/image5', '/image6', 80, '2022-01-15', True, 'Canada', 'London', 'ON');
-
-
-INSERT INTO features (user_id, item_id)
-VALUES(1,1);
-INSERT INTO features (user_id, item_id)
-VALUES (1,3);
-INSERT INTO features (user_id, item_id)
-VALUES (2,3);
-INSERT INTO features (user_id, item_id)
-VALUES(4,2);
-INSERT INTO features (user_id, item_id)
-VALUES (3,3);
-INSERT INTO features (user_id, item_id)
-VALUES (3,4);
-INSERT INTO features (user_id, item_id)
-VALUES(3,2);
-INSERT INTO features (user_id, item_id)
-VALUES (3,3);
+INSERT INTO items (admin_id, title,description, thumbnail_photo_url, cover_photo_url, cost, date, sold, country, city, province)
+VALUES (6, 'shirt', 'graphic text' , '/image5', '/image6', 80, '2022-01-15', True, 'Canada', 'London', 'ON');
 
 
 INSERT INTO messages (user_id, item_id, message)
@@ -72,3 +63,38 @@ VALUES (3,2,'and will you do 40?');
 
 INSERT INTO messages (user_id, item_id, message)
 VALUES (1,3, 'is it still availible?');
+
+
+INSERT INTO user_favorites (user_id, item_id)
+VALUES(1,1);
+INSERT INTO user_favorites (user_id, item_id)
+VALUES (1,3);
+INSERT INTO user_favorites (user_id, item_id)
+VALUES (2,3);
+INSERT INTO user_favorites (user_id, item_id)
+VALUES(2,2);
+INSERT INTO user_favorites (user_id, item_id)
+VALUES (3,3);
+INSERT INTO user_favorites (user_id, item_id)
+VALUES (3,6);
+INSERT INTO user_favorites (user_id, item_id)
+VALUES(3,5);
+INSERT INTO user_favorites (user_id, item_id)
+VALUES (4,1);
+INSERT INTO user_favorites (user_id, item_id)
+VALUES(5,1);
+INSERT INTO user_favorites (user_id, item_id)
+VALUES (5,3);
+INSERT INTO user_favorites (user_id, item_id)
+VALUES (7,3);
+INSERT INTO user_favorites (user_id, item_id)
+VALUES(8,2);
+INSERT INTO user_favorites (user_id, item_id)
+VALUES (9,3);
+INSERT INTO user_favorites (user_id, item_id)
+VALUES (10,6);
+INSERT INTO user_favorites (user_id, item_id)
+VALUES(12,2);
+INSERT INTO user_favorites (user_id, item_id)
+VALUES (10,5);
+
