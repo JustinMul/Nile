769a0d08e6cb4c8f438bac3b5bb2ce299c6bce17
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-03-14 14:42:01 -0400

    Add cookie

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router  = express.Router();
+const database = require('../HelperFunctions/getUserEmail.js');
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    res.render("index");
+    const accountEmail = req.session.user_id;
+    console.log("accountemail cookie",accountEmail);
+    database.getName(accountEmail).then((value) => {
+      console.log("TEST NAME: ", value);
+      const templateVars = {value};
+      res.render("index", templateVars);
+    });
   });
   return router;
 };
