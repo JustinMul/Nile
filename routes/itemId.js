9db4e9bc6aac230a77e18c4db673b05ba5eff224
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-03-14 20:56:13 -0400

    fix indents

--- routes/itemId.js
+++ routes/itemId.js
@@ -8,22 +8,22 @@ module.exports = (db) => {
     const itemId = req.params.itemid;
 
     const accountEmail = req.session.user_id;
-  const is_admin = req.session.is_admin;
-  console.log("accountemail cookie",accountEmail);
-  database.getName(accountEmail).then((value) => {
-    console.log("TEST NAME: ", value);
-    db.query(`SELECT * FROM items WHERE id = $1`, [itemId])
-    .then(data => {
-      const templateVars = {item: data.rows[0], value, is_admin};
-      res.render("itemid", templateVars);
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
+    const is_admin = req.session.is_admin;
+    console.log("accountemail cookie",accountEmail);
+    database.getName(accountEmail).then((value) => {
+      console.log("TEST NAME: ", value);
+      db.query(`SELECT * FROM items WHERE id = $1`, [itemId])
+        .then(data => {
+          const templateVars = {item: data.rows[0], value, is_admin};
+          res.render("itemid", templateVars);
+        })
+        .catch(err => {
+          res
+            .status(500)
+            .json({ error: err.message });
+        });
     });
-  })
   });
 
- return router;
-}
+  return router;
+};
