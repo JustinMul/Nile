e57b8196608d7c13a98d00c9e7f749177d7c6d35
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-03-15 17:57:19 -0400

    Add user_id and item_id to user favourites table

--- routes/favourites.js
+++ routes/favourites.js
@@ -3,35 +3,34 @@ const router  = express.Router();
 const database = require('../HelperFunctions/getUserEmail.js');
 const itemGet = require('../HelperFunctions/getAllItems.js');
 const itemInsert = require('../HelperFunctions/insertItem.js');
-itemInsert.insertCookieItemId;
+
 module.exports = (db) => {
   router.get("/favourites", (req, res) => {
     const cookieItemId = req.session.itemid;
     const accountEmail = req.session.user_id;
     const is_admin = req.session.is_admin;
-    database.getUserId(accountEmail)
+    database.getUserId(accountEmail) // gets user id with email
       .then((val) => {
-        const arr = [val.id, cookieItemId];
-        database.getUserFavCheck(arr)
+        const arr = [val.id, cookieItemId]; // user id and item it
+        database.getUserFavCheck(arr) // check if favourties duplicates
           .then((s)=> {
             if (Number(s[0].count) < 1) {
-              itemInsert.insertCookieItemId(arr);
+              itemInsert.insertCookieItemId(arr); // if no duplicates insert into user favourites database
             }
           });
-        database.getName(accountEmail).then((value) => {
-          db.query(`SELECT item_id FROM user_favorites WHERE user_id = $1;`, [arr[0]])
+        database.getName(accountEmail).then((value) => { // Gets account name with email to render navbar
+          //db.query(`SELECT item_id FROM user_favorites WHERE user_id = $1;`, [arr[0]])
+
+
+          db.query(`SELECT user_favorites.user_id, user_favorites.item_id, user_favorites.id, items.*
+          FROM user_favorites
+          JOIN items
+          ON user_favorites.item_id = items.id
+         AND user_favorites.user_id = $1;`, [arr[0]])
             .then(data => {
-              console.log("datadatadatadatadatadata", data.rows[0].item_id);
-              const arrList = [];
-              for (let i = 0; i < data.rows.length; i++) {
-                let itemIdList = data.rows[i].item_id;
-                itemGet.getUserFavourite(itemIdList)
-                  .then((v)=> {
-                    arrList.push(v);
-                  });
-              }
-              console.log("arrListarrListarrListarrListarrListarrListarrListarrListarrListarrListarrListarrListarrListarrListarrListarrListarrListarrListarrList",arrList);
-              const tempVar = {item:data.rows,value, accountEmail, is_admin};
+              console.log("data rows:", data.rows);
+
+              const tempVar = {value, accountEmail, is_admin, items: data.rows};
               res.render("favourites", tempVar);
             });
         });
@@ -43,4 +42,9 @@ module.exports = (db) => {
   });
   return router;
 };
+
+
+
+
+
 
