e6850a107efd669c3a7fde0a66c576ae32ad7703
Justin Mulroney <your@email.comjustinmulroney@gmail.com>
2022-03-15 21:06:18 -0400

    bug fix for user :id pages

--- routes/itemId.js
+++ routes/itemId.js
@@ -11,19 +11,26 @@ module.exports = (db) => {
     console.log(cookieItemId);
     const accountEmail = req.session.user_id;
     const is_admin = req.session.is_admin;
-    const adminEmail = req.session.user_id;
+    const adminEmail = req.session.adminEmail;
+
     console.log('this is req.sessions', req.session)
-    console.log('this is admin emailemailemailemailemailemail', adminEmail)
+    console.log('this is admin emailemailemailemailemailemail', adminEmail);
 
     database.getName(accountEmail).then((value) => {
       console.log("TEST NAME: ", value);
       db.query(`SELECT * FROM items WHERE id = $1`, [itemId])
         .then(data => {
           database.getAdminId(adminEmail).then((adminIdValue)=>{
-            const adminId = adminIdValue.id;
-            console.log("datadatadatadatadatadatadatadatadatadata", adminId);
-            const templateVars = {item: data.rows[0], value, is_admin, cookieItemId, adminId };
-            res.render("itemid", templateVars);
+            if (!adminIdValue) {
+              adminId = null;
+              const templateVars = {item: data.rows[0], value, is_admin, cookieItemId, adminId};
+              res.render("itemid", templateVars);
+            } else {
+              const adminId = adminIdValue.id;
+              console.log("datadatadatadatadatadatadatadatadatadata", adminId);
+              const templateVars = {item: data.rows[0], value, is_admin, cookieItemId, adminId};
+              res.render("itemid", templateVars);
+            }
           });
         })
         .catch(err => {
