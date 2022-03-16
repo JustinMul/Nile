fb90aff452d814990fd61dc7b95e9e5cd9fba207
Justin Mulroney <your@email.comjustinmulroney@gmail.com>
2022-03-15 21:20:07 -0400

    fix bug for listings

--- routes/listings.js
+++ routes/listings.js
@@ -11,43 +11,32 @@ const pool = new Pool({
 });
 
 let adminId;
+
 //now working need to get visual output!
 module.exports = (db) => {
   router.get("/listings", (req, res) => {
+    console.log('this is ressssss', req.session);
+    console.log("res.session.is_adminres.session.is_admin",req.session.is_admin);
+    if (!req.session.is_admin) {
+      res.redirect('/');
+    } else {
+      pool
+        .query(`SELECT id FROM admins WHERE email = $1`, [req.session.user_id])
+        .then((data) => {
+          adminId = data.rows[0];
+        }).then((data) => {
+          pool.query(`SELECT * FROM items WHERE admin_id = $1;`, [adminId.id])
+            .then((results) => {
 
-    pool
-      .query(`SELECT id FROM admins WHERE email = $1`, [req.session.user_id])
-      .then((data) => {
-        adminId = data.rows[0];
-      }).then((data) => {
-        pool.query(`SELECT * FROM items WHERE admin_id = $1;`, [adminId.id])
-          .then((results) => {
-
-            const value = req.session.user_id;
-            const is_admin = req.session.is_admin;
-            const tempVar = {items: results.rows, value, is_admin};
-            console.log('this is temp vars', tempVar);
-            res.render('listing', tempVar);
-          });
-      });
-
+              const value = req.session.user_id;
+              const is_admin = req.session.is_admin;
+              const tempVar = {items: results.rows, value, is_admin};
+              console.log('this is temp vars', tempVar);
+              res.render('listing', tempVar);
+            });
+        });
+    }
   });
 
-  // router.post("/items/:id/delete", (req, res) => {
-  //   pool
-  //     .query(`SELECT id FROM admins WHERE email = $1`, [req.session.user_id])
-  //     .then((data) => {
-  //       console.log('inhere?');
-  //       adminId = data.rows[0];
-  //     }).then((data) => {
-
-  //       pool.query(`DELETE * FROM items WHERE admin_id = $1;`, [adminId.id])
-  //         .then((data) => {
-  //           console.log('it was deleted');
-  //           res.redirect('listing');
-  //         });
-
-  //     });
-  // });
   return router;
 };
