ec555602dbc5e7207593a2ff1d45b1aaf73dd9bb
Justin Mulroney <your@email.comjustinmulroney@gmail.com>
2022-03-15 21:57:00 -0400

    add login dupe prevention

--- routes/login.js
+++ routes/login.js
@@ -10,16 +10,22 @@ const database = require('../HelperFunctions/getUserEmail.js');
 
 module.exports = (db) => {
 
+
   router.get("/login", (req, res) => {
-    console.log("cookie session for GET TEST: ", req.session.user_id);
-    const accountEmail = req.session.user_id;
-    const is_admin = req.session.is_admin;
-    console.log("accountemail cookie",accountEmail);
-    database.getName(accountEmail).then((value) => {
-      console.log("TEST NAME: ", value);
-      const templateVars = {value, is_admin};
-      res.render("login", templateVars);
-    });
+
+    if (req.session.user_id === undefined) {
+      const accountEmail = req.session.user_id;
+      const is_admin = req.session.is_admin;
+      console.log("accountemail cookie",accountEmail);
+      database.getName(accountEmail).then((value) => {
+        console.log("TEST NAME: ", value);
+        const templateVars = {value, is_admin};
+        res.render("login", templateVars);
+      });
+    } else {
+      console.log('nonononononon')
+      res.redirect('/');
+    }
 
   });
 
